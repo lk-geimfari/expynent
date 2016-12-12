4ee63e24f9aa3c4c614864cd85bda666ef3c5905
Likid Geimfari <likid.geimfari@gmail.com>
2016-12-12 11:35:02 +0300

    Update

--- expynent/__init__.py
+++ expynent/__init__.py
@@ -1,3 +1,9 @@
+"""
+:copyright: (c) 2016 by Isaak Uchakaev <likid.geimfari@gmail.com>.
+:software_license: BSD 3-Clause, see LICENSES for more details.
+:repository: https://github.com/lk-geimfari/expynent
+"""
+
 from .patterns import *
 
 __version__ = '0.0.2'
