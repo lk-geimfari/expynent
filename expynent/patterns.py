ef2238859a729ddf40cf778114cc5a86be2698d3
Panagiotis Simakis <sp1thas@autistici.org>
2018-10-14 16:03:05 +0300

    Add pattern for greek license plates (#69)
    * add pattern and test for greek license plates
    
    * add empty lines
    
    * use generator instead of list in test

--- expynent/patterns.py
+++ expynent/patterns.py
@@ -320,7 +320,9 @@ LICENSE_PLATE = {
     # Regex pattern to match Taiwanese license plates
     'TW': r'(^[A-Z0-9]{2,3}-\d{2,4}$)|(^\d{2,3}-[A-Z0-9]{2,3}$)|(^\d{4}-[A-Z0-9]{2}$)|',
     # Regex pattern to match French license plates
-    'FR': r'(^[A-Z]{2}-\d{3}-[A-Z]{2}$)|(^\d{1,4}\s[A-Z]{1,3}\s\d{2}$)'
+    'FR': r'(^[A-Z]{2}-\d{3}-[A-Z]{2}$)|(^\d{1,4}\s[A-Z]{1,3}\s\d{2}$)',
+    # Regex pattern to match Greek license plates
+    'GR': r'(^[ABEZHIKMNOPTYXΑΒΕΖΗΙΚΜΝΟΡΤΥΧ]{3}-\d{4})',
 }
 
 # RegEx pattern for matching 24 hour time format.
