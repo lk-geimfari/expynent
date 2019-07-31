bdf511c70a4730b4589d3f4d8b27903ff18008fe
Líkið Geimfari <likid.geimfari@gmail.com>
2019-07-31 21:40:42 +0300

    Update dependencies

--- expynent/__init__.py
+++ expynent/__init__.py
@@ -4,7 +4,7 @@ License: BSD 3-Clause, see LICENSES for more details.
 Repository: https://github.com/lk-geimfari/expynent
 """
 
-from .patterns import *
+from expynent.patterns import *
 
-__version__ = '2.0.0'
+__version__ = '2.1.0'
 __author__ = 'Likid Geimfari'
